60820697a2fed7e1e05b8c060b62a31804c43ba5
Marina Fedkova <mzaloznaya@ya.ru>
2021-04-20 14:37:35 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import java.io.FileInputStream
+
+
+fun main() {
+    val options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
+        .build()
+
+    FirebaseApp.initializeApp(options)
+
+    val message = Message.builder()
+        .putData("action", "NEW_POST")
+        .putData("content", """{          
+          "userName": "Vasiliy",         
+          "text": "Таймбоксинг — отличный способ навести порядок в своём календаре и разобраться с делами, которые долго откладывали на потом. Его главный принцип — на каждое дело заранее выделяется определённый отрезок времени. В это время вы работаете только над одной задачей, не переключаясь на другие. Собрали советы, которые помогут внедрить таймбоксинг"
+        }""".trimIndent())
+        .setToken(token)
+        .build()
+
+//    val message = Message.builder()
+//        .putData("action", "LIKE")
+//        .putData("content", """{
+//          "userId": 1,
+//          "userName": "Vasiliy",
+//          "postId": 2,
+//          "postAuthor": "Netology"
+//        }""".trimIndent())
+//        .setToken(token)
+//        .build()
+
+    FirebaseMessaging.getInstance().send(message)
+}
